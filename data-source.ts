55273939b1b0e5928e9d72cdd54de36ea377262b
rizkynear <rizkynear1@gmail.com>
2022-08-04 11:44:21 +0800

    implement migration

--- data-source.ts
+++ data-source.ts
@@ -1,10 +1,14 @@
-module.exports = {
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    seeds: ['database/seeders/**/*{.ts,.js}'],
-    factories: ['database/factories/**/*{.ts,.js}']
-};
+    migrations: ['database/migrations/**/*{.ts,.js}']
+});
